6133a0cadd508309ea66cfcda017865334d80b2c
Mariia Korol <marie.korol1991@gmail.com>
2023-06-19 19:30:10 +0300

    add layout messageList

--- src/components/MessagesList/OneMessage.jsx
+++ src/components/MessagesList/OneMessage.jsx
@@ -1,6 +1,13 @@
-export function OneMessage({id, content, ownerId, ownerName, createdAt}) {
+import { memo } from "react";
+
+export const OneMessage = memo(({ content, ownerName, createdAt }) => {
+    const time = new Date(createdAt).toLocaleTimeString([], { timeStyle: 'short' });
 
     return (
-        <div>OneMessage</div>
+        <li className="border border-gray-300 rounded bg-gray-100 p-2 block w-max relative">
+            <span className="font-bold"> {ownerName} </span>
+            <p> {content} </p>
+            <span className="absolute bottom-0 right-0 right-[-3rem]"> {time} </span>
+        </li>
     );
-}
+})
