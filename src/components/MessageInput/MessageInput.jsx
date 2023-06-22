89b9e86d5e979043edc3bf8c032121c626b7082c
Mariia Korol <marie.korol1991@gmail.com>
2023-06-22 15:27:58 +0300

    removed console.logs

--- src/components/MessageInput/MessageInput.jsx
+++ src/components/MessageInput/MessageInput.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 import { AiOutlinePaperClip } from "react-icons/ai";
 import { BiSmile } from "react-icons/bi";
 import { BsSend } from "react-icons/bs";
 
-export const MessageInput = () => {
+export const MessageInput = memo(() => {
     const [message, setMessage] = useState('');
-    console.log(message);
 
     const onChangeHandler = (event) => {
         setMessage(event.target.value);
@@ -36,24 +35,24 @@ export const MessageInput = () => {
                 <div className="flex items-center space-x-2">
                     <button
                         className="px-4 py-2 rounded text-xl"
-                        onClick={() => console.log('smile')}
+                    // onClick={() => console.log('smile')}
                     >
                         <BiSmile />
                     </button>
                     <button
                         className="px-4 py-2 rounded text-xl"
-                        onClick={() => console.log('add file')}
+                    // onClick={() => console.log('add file')}
                     >
                         <AiOutlinePaperClip />
                     </button>
                 </div>
                 <button
                     className="px-4 py-2 rounded mt-2 border bg-white flex items-center"
-                    onClick={() => console.log('send')}
+                // onClick={() => console.log('send')}
                 >
                     <BsSend className="text-xl" />
                 </button>
             </div>
         </div>
     )
-}
+});
