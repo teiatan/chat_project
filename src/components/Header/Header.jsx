6f590cdd79779f622877d272124ea29106cf136f
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-25 00:34:32 +0300

    add positioning and plugs to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,15 @@
 export const Header = ({userName}) => {
     return (
         <div className="fixed w-screen h-[80px] z-10 bg-[#D9D9D9] text-2xl">
-            <div className="w-[1000px] h-full flex items-center justify-between m-auto">
-                <div>LOGO</div>
+            <div className="h-full flex items-center justify-between m-auto">
+                <div className="w-[345px] text-center">LOGO</div>
 
-                <div>
-                    <button>Theme button</button>
+                <div className="mr-[100px] flex gap-[30px]">
+                    <button className="rounded-full bg-white p-2">Theme button</button>
 
-                    {true ?
-                        <button>{userName}</button> :
-                        <button>Language button</button>
+                    {userName === '' ?
+                        <button className="min-w-[223px]">UA/ENG</button>:
+                        <button className="rounded-full bg-white p-2 min-w-[223px]">{userName}</button>
                     }
                 </div>
             </div>
