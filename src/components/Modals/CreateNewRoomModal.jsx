7d8765e38cc0791e890a4134172517ebbe6458ba
teiatan <92535482+teiatan@users.noreply.github.com>
2023-06-30 20:26:05 +0300

    Update CreateNewRoomModal.jsx

--- src/components/Modals/CreateNewRoomModal.jsx
+++ src/components/Modals/CreateNewRoomModal.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { ModalCover } from "./ModalCover";
-import { BsEyeSlash } from "react-icons/bs";
+import { ModalCover } from './ModalCover';
+import { BsEyeSlash } from 'react-icons/bs';
 
 export const CreateNewRoomModal = ({ onClose }) => {
     const [roomName, setRoomName] = useState('');
@@ -9,15 +9,15 @@ export const CreateNewRoomModal = ({ onClose }) => {
     const [isPrivate, setIsPrivate] = useState(false);
     const [passwordType, setPasswordType] = useState('password');
 
-    const handleRoomNameChange = (e) => {
+    const handleRoomNameChange = e => {
         setRoomName(e.target.value);
     };
 
-    const handleBackgroundColorChange = (e) => {
+    const handleBackgroundColorChange = e => {
         setBackgroundColor(e.target.value);
     };
 
-    const handleTextColorChange = (e) => {
+    const handleTextColorChange = e => {
         setTextColor(e.target.value);
     };
 
@@ -32,7 +32,7 @@ export const CreateNewRoomModal = ({ onClose }) => {
     return (
         <ModalCover onClose={onClose}>
             <div className="flex flex-col gap-5 w-3/4">
-                <p className='text-center'>Нова кімната</p>
+                <p className="text-center">Нова кімната</p>
                 <input
                     type="text"
                     className="w-full px-4 py-2 border rounded"
@@ -40,44 +40,48 @@ export const CreateNewRoomModal = ({ onClose }) => {
                     value={roomName}
                     onChange={handleRoomNameChange}
                 />
-                <div className='flex justify-around'>
+                <div className="flex justify-around">
                     <div>
                         <input
                             type="radio"
                             id="public"
-                            name='typeroom'
+                            name="typeroom"
                             checked={!isPrivate}
                             onChange={() => setIsPrivate(false)}
                         />
-                        <label htmlFor="public">
-                            Публічна
-                        </label>
+                        <label htmlFor="public">Публічна</label>
                     </div>
                     <div>
                         <input
                             type="radio"
                             id="private"
-                            name='typeroom'
+                            name="typeroom"
                             checked={isPrivate}
                             onChange={() => setIsPrivate(true)}
                         />
-                        <label htmlFor="private">
-                            Приватна
-                        </label>
+                        <label htmlFor="private">Приватна</label>
                     </div>
                 </div>
-                {isPrivate &&
-                    <div className='relative'>
+                {isPrivate && (
+                    <div className="relative">
                         <p>Створіть ключ для приватної кімнати</p>
-                        <input type={passwordType} className="w-full px-4 py-2 border rounded" />
-                        <BsEyeSlash onClick={togglePassword} className='absolute bottom-8 right-2 cursor-pointer' />
-                        <p class="text-sm">*Після створення кімнати ключ змінити не можна</p>
+                        <input
+                            type={passwordType}
+                            className="w-full px-4 py-2 border rounded"
+                        />
+                        <BsEyeSlash
+                            onClick={togglePassword}
+                            className="absolute bottom-8 right-2 cursor-pointer"
+                        />
+                        <p classNmae="text-sm">
+                            *Після створення кімнати ключ змінити не можна
+                        </p>
                     </div>
-                }
+                )}
                 <p>Оберіть вид вашой кімнати</p>
                 <div className="flex justify-between">
                     <div className="flex flex-col">
-                        <div class="flex items-center gap-8">
+                        <div className="flex items-center gap-8">
                             <span>Колір фону</span>
                             <input
                                 type="color"
@@ -86,7 +90,7 @@ export const CreateNewRoomModal = ({ onClose }) => {
                                 onChange={handleBackgroundColorChange}
                             />
                         </div>
-                        <div class="flex items-center gap-8">
+                        <div className="flex items-center gap-8">
                             <span>Колір тексту</span>
                             <input
                                 type="color"
@@ -96,21 +100,24 @@ export const CreateNewRoomModal = ({ onClose }) => {
                             />
                         </div>
                     </div>
-                    <div className='flex'>
+                    <div className="flex">
                         <span
                             className="px-4 py-2 border rounded flex-grow min-w-[80px] font-bold"
-                            style={{ color: textColor, backgroundColor: backgroundColor }}
+                            style={{
+                                color: textColor,
+                                backgroundColor: backgroundColor,
+                            }}
                         >
                             {roomName.toLocaleUpperCase()}
                         </span>
                     </div>
                 </div>
-                <div className='flex justify-center'>
-                    <button className="px-4 py-2 border rounded bg-black text-white" >
+                <div className="flex justify-center">
+                    <button className="px-4 py-2 border rounded bg-black text-white">
                         Створити кімнату
                     </button>
                 </div>
             </div>
         </ModalCover>
-    )
-}
+    );
+};
